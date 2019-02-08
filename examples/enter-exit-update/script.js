6437fc23627d2f3f691d83c99273306f4a51d80a
siqister <siqi.zhu@gmail.com>
2019-02-07 21:47:31 -0500

    Add example

---
+++ examples/enter-exit-update/script.js
@@ -0,0 +1,121 @@
+const w = d3.select('.plot').node().clientWidth;
+const h = d3.select('.plot').node().clientHeight;
+
+//Prepare <svg> canvases
+const plot1 = d3.select('#plot-1').append('svg').attr('width',w).attr('height',h);
+const plot2 = d3.select('#plot-2').append('svg').attr('width',w).attr('height',h);
+const plot3 = d3.select('#plot-3').append('svg').attr('width',w).attr('height',h);
+const plot4 = d3.select('#plot-4').append('svg').attr('width',w).attr('height',h);
+
+updatePlot1(generateSimpleData());
+updatePlot3(generateNestedData());
+
+//Event listeners
+d3.select('#generate-simple-data-btn')
+	.on('click', () => {
+
+		console.group('Update simple data');
+		
+		const data = generateSimpleData();
+		console.log(data);
+		updatePlot1(data);
+		updatePlot2(data); //YOU NEED TO COMPLETE THIS FUNCTION
+
+		console.groupEnd();
+
+	});
+
+d3.select('#generate-nested-data-btn')
+	.on('click', () => {
+
+		console.group('Update nested data');
+
+		const data = generateSimpleData();
+		updatePlot3(data);
+		updatePlot4(data); //YOU NEED TO COMPLETE THIS FUNCTION
+
+		console.groupEnd();
+
+	});
+
+function updatePlot1(data){
+
+	//UPDATE SELECTION
+	const nodes = plot1.selectAll('.node')
+		.data(data, d => d.name);
+	nodes.select('circle')
+		//.transition()
+		.style('fill','#aa0000'); //circles in the UPDATE selection are black; note the use of .transition
+
+	//ENTER SELECTION
+	const nodesEnter = nodes.enter()
+		.append('g').attr('class', 'node');
+	nodesEnter.append('circle')
+		.style('fill', '#03afeb'); //circles in the ENTER selection are colored differently
+	nodesEnter.append('text')
+		.attr('text-anchor', 'middle');
+
+	//UPDATE + ENTER
+	//Both of their 'transform' attributes are the set the same way
+	//so we merge them
+	nodes.merge(nodesEnter)
+		.transition()
+		.attr('transform', d => `translate(${d.x}, ${d.y})`);
+	nodes.merge(nodesEnter)
+		.select('text')
+		.text(d => d.name);
+	nodes.merge(nodesEnter)
+		.select('circle')
+		.transition()
+		.attr('r', d => d.value);
+
+	//EXIT SELECTION
+	nodes.exit().remove();
+}
+
+function updatePlot2(data){
+	//YOUR CODE HERE
+}
+
+function updatePlot3(data){
+}
+
+function updatePlot4(data){
+	//YOUR CODE HERE
+}
+
+
+//Functions for generating updated data
+//You don't need to know this, 
+//but do make sure you understand what the output of these functions look like
+function generateSimpleData(){
+	const entities = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+	let subset = new Set();
+
+	//Randomly shuffle a subset of the entities
+	for(let i = 0; i < entities.length; i++){
+		subset.add(entities[Math.floor(Math.random()*entities.length)]);
+	}
+
+	return Array.from(subset)
+		.map(d => ({
+			name: d,
+			x: Math.random()*w,
+			y: Math.random()*h,
+			value: Math.random()*20+5
+		}))
+}
+
+function generateNestedData(){
+	return [
+		{
+			id: 'team-1',
+			values: generateSimpleData()
+		},
+		{
+			id: 'team-2',
+			values: generateSimpleData()
+		}
+	]
+}
+
